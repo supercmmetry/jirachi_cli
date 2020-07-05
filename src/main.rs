e4a518c7f4277c1c8ea5a6c2900cf5e44e59dfe0
supercmmetry <vishaals2000@gmail.com>
2020-07-05 09:13:12 +0530

    jirachi: Implement setup
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod seed;
 
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
 
 use schema::seeds::dsl::*;
 use diesel::{RunQueryDsl, PgConnection, Connection};
@@ -10,12 +12,20 @@ use dotenv;
 use std::env;
 use crate::seed::Seed;
 
+embed_migrations!("migrations");
+
 fn add(conn: &PgConnection, other_prefix: String) -> anyhow::Result<()> {
     diesel::insert_into(seeds)
         .values(&Seed {
             prefix: other_prefix,
             index: 0
         }).execute(conn)?;
+
+    Ok(())
+}
+
+fn setup(conn: &PgConnection) -> anyhow::Result<()> {
+    embedded_migrations::run(conn)?;
 
     Ok(())
 }
@@ -55,6 +65,11 @@ fn main() {
 
     match args[1].as_str() {
         "add" => {
+            if args.len() < 3 {
+                print_usage();
+                return;
+            }
+
             for i in 2..args.len() {
                 if let Err(e) = add(&conn, args[i].clone()) {
                     println!("error: Could not add key \n{}", e);
@@ -62,6 +77,14 @@ fn main() {
                 }
                 println!("success: Added key {} to jirachi database.", args[i]);
             }
+        },
+        "setup" => {
+            if let Err(e) = setup(&conn) {
+                println!("error: Could not setup database \n{}", e);
+                return;
+            }
+
+            println!("success: Initialized jirachi database");
         },
         _ => print_usage()
     }
